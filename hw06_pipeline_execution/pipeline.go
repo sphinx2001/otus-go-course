157d320fe9b5c15100f13596afd53754077f151b
sphinx <k.komandin@gmail.com>
2026-01-21 23:23:55 +0500

    частичное прохождение тестов

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,55 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		// если стадий нет, мы не можем закрыть in, поэтому создаем прокси канал и возвращаем его
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for {
+				select {
+				case val, ok := <-in:
+					if !ok {
+						return
+					}
+					select {
+					case out <- val:
+					case <-done:
+						return
+					}
+				case _, _ = <-done:
+					return
+				}
+			}
+		}()
+		return out
+	}
+
+	currentIn := in
+	for _, stage := range stages {
+		currentIn = stage(currentIn)
+	}
+
+	result := make(Bi)
+
+	go func() {
+		defer close(result)
+		for {
+			select {
+			case val, ok := <-currentIn:
+				if !ok {
+					return
+				}
+				select {
+				case result <- val:
+				case _, _ = <-done:
+					return
+				}
+			case _, _ = <-done:
+				return
+			}
+		}
+	}()
+
+	return result
 }
