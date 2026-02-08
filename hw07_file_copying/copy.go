64d0105f3cce5065d6c1413ab35fcbe2dc25f931
sphinx <k.komandin@gmail.com>
2026-02-08 17:59:24 +0500

    fix linter

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -41,12 +41,9 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	bar := pb.Full.Start64(limit)
 	barReader := bar.NewProxyReader(limitReader)
 
-	writer, err := os.OpenFile(toPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	writer, err := os.OpenFile(toPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o666)
 	if err != nil {
 		return err
-	}
-	if limit < 0 {
-
 	}
 
 	defer writer.Close()
