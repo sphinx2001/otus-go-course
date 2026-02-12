310f89cc8f26858cce06a7450e8f2c0e991e053c
sphinx <k.komandin@gmail.com>
2026-02-12 20:05:46 +0500

    промежуточное сохранение

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Empty args...")
+		return
+	}
+	envs, err := ReadDir(args[0])
+	if err != nil {
+		fmt.Printf("ReadDir error: %v\n", err.Error()+"\n")
+		return
+	}
+
+	args = args[1:]
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Env = []string{}
+	var out strings.Builder
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(out.String())
 }
